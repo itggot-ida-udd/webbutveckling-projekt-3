bc110bc6cc93a081f6f1a36e96bd5228a8494654
itggot-ida-udd <ida.udd@elev.ga.ntig.se>
2019-03-28 23:01:25 +0100

    js
    ..

--- Ny mapp/site/js/main.js
+++ Ny mapp/site/js/main.js
@@ -1,1 +1,11 @@
-$('')
+
+$(".menu").hide();
+
+$("#close").click(function(){
+    $(this).parents().find(".menu").hide(200);
+  });
+
+
+$("#hamburger-menu").click(function(){
+   console.log( $(this).parents().find(".menu").show(200));
+  });  
